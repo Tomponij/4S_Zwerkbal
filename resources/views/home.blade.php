f3f38ca1358d8c51ce042e9589f5dd56549ce774
Tomponij <tomslavenburg@gmail.com>
2021-09-23 14:28:33 +0200

    font

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,8 @@
+<!--FONTS-->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;700&display=swap" rel="stylesheet">
+
 @extends('layout')
 
 @section('nav')
@@ -11,7 +16,19 @@
 
 @section('content')
 
-    <h1>Test</h1>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus, eligendi porro! Asperiores odio deleniti voluptatibus obcaecati corporis tempora quos numquam molestiae! Dolorum quis hic doloribus debitis nesciunt minima, quisquam optio.</p>
-
+    <h1>Dashboard</h1>
+    <p>Zwerkbal (Engels: Quidditch) is een sport voor tovenaars uit de Harry Potterboeken van Joanne Rowling. In de wereld van Harry Potter is Zwerkbal een populaire sport: veel mensen praten erover mee en zijn zeer enthousiast als er een groot toernooi wordt gespeeld. Ook heeft iedere heks of tovenaar wel een favoriet Zwerkbalteam. Het idee van Zwerkbal lijkt te zijn gebaseerd op het non-fictieve horseball.</p>
+    <div class="grid-containerIndex">
+        <div class="indexInfo"> 
+            <p>Aantal komende toernooien:</p>
+            <p>5</p>
+        </div>
+        <div class='indexIMG'>
+            <img src="/img/snitch.png">
+        </div>
+        <div class="indexInfo2">
+            <p>Totaal aantal teams:</p>
+            <p>26</p> 
+        </div>
+    </div>
 @endsection
--- resources/views/tournaments/index.blade.php
+++ resources/views/tournaments/index.blade.php
@@ -12,7 +12,9 @@
 @section('content')
 
     <h1>Toernooien</h1>
-    <a href="{{ route('tournaments.create') }}">+ nieuw toernooi</a>
+    <div class="nieuwToernooi">
+        <a href="{{ route('tournaments.create') }}">+ nieuw toernooi</a>
+    </div>
     <table class="table">
         <tr>
             <th>Toernooi</th>
@@ -22,6 +24,7 @@
         @foreach($tournaments as $tournament)
             <tr>
                 <td>{{ $tournament->name }}</td>
+                <td>{{ $tournament->date }}</td>
                 <td><a href="{{ route('tournaments.edit', $tournament->id) }}">Aanpassen</a></td>
             </tr>
         @endforeach
